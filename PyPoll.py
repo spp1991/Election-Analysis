206857cb500624dbe2286d7dd4b790958cee00db
Sunny Patel <spp1991@gmail.com>
2021-07-04 19:41:13 -0400

    Added data file and initial analysis code

---
+++ PyPoll.py
@@ -0,0 +1,27 @@
+import csv
+import os
+
+# Assign a variable for the file to load and the path.
+file_to_load = os.path.join("Resources", "election_results.csv")
+
+# Create a filename variable to a direct or indirect path to the file.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# Open the election results and read the file
+with open(file_to_load) as election_data:
+
+    # To do: perform analysis.
+    # Read the file object with the reader function.
+    file_reader = csv.reader(election_data)
+    
+    # Print the header row.
+    headers = next(file_reader)
+    print(headers)
+
+
+
+# Using the with statement open the file as a text file.
+with open(file_to_save, "w") as txt_file:
+
+    # Write some data to the file.
+    txt_file.write("Hello World")
